85d4e995afddfc648a0281f37541b0ba1c5b4fb9
Auldriya <auldriyadcunha@gmail.com>
2025-07-15 07:58:59 +0530

    project created

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import StudentNavbar from './StudentNavbar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <StudentNavbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="label form-label">Name</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="label form-label">College</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="label form-label">Roll Number</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="label form-label">Admission Number</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="button btn btn-success">Submit</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddStudent
