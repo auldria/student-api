6115e324a0b309b27a137825b8835e27adbe784e
Auldriya <auldriyadcunha@gmail.com>
2025-07-16 09:18:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import SearchStudent from './components/SearchStudent';
 import DeleteStudent from './components/DeleteStudent';
 import ViewStudent from './components/ViewStudent';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginStudent from './components/LoginStudent';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginStudent/>}/>
       <Route path="/" element={<AddStudent/>}/>
       <Route path="/AddStudent" element={<AddStudent/>}/>
       <Route path="/SearchStudent" element={<SearchStudent/>}/>
       <Route path="/DeleteStudent" element={<DeleteStudent/>}/>
       <Route path="/ViewStudent" element={<ViewStudent/>}/>
+      <Route path="/login" element={<LoginStudent/>}/>
       </Routes>
       </BrowserRouter>
     </div>
